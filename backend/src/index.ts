ca3481d186167a4f4a2855b963fe0c3fd93511a8
Priyanshu1805 <priyanshutalvekar@email.com>
2026-05-22 03:10:10 +0530

    Allow all CORS origins

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,11 +10,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors(
-  {
-    origin: "https://solarx-front-end.vercel.app"
-  }
-));
+app.use(cors());
 app.use(express.json());
 
 // Routes
