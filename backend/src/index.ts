44affc5c313e2bbb17574796e2b9ffc0e2b59cd2
Priyanshu1805 <priyanshutalvekar@email.com>
2026-05-22 01:54:56 +0530

    Update CORS origin for Vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,11 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: "https://solarx-front-end.vercel.app"
+  }
+));
 app.use(express.json());
 
 // Routes
